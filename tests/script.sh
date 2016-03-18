516b23b141e189c9bbedb8dab2f23c952dd84c7b
Radovan Sroka <rsroka@redhat.com>
2016-03-18 02:05:36 +0100

    Continues integration

---
+++ tests/script.sh
@@ -0,0 +1,19 @@
+#~/bin/bash
+
+#set -x
+
+
+cp ../proj1.php ./cls.php
+cp ~/jexamxml.jar ./
+./_stud_tests.sh
+
+for  i in `seq 1 13` ;
+do
+if [ $i -lt 10 ]; then
+	java -jar ./jexamxml.jar test0$i.out ref-out/test0$i.out
+else 
+	java -jar ./jexamxml.jar test$i.out ref-out/test$i.out
+fi
+done
+
+rm -rf ./cls.php
